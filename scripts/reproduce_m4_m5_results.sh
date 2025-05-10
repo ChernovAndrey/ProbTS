3fb008d8a924fd66e23c3a91fa314151b6d995f7
Andrei Chernov <chernov.andrey.998@gmail.com>
2025-05-10 12:08:53 +0200

    added the script for the m5 dataset

--- scripts/reproduce_m4_m5_results.sh
+++ scripts/reproduce_m4_m5_results.sh
@@ -5,7 +5,7 @@ LOG_DIR=./exps
 
 for DATASET in 'm5'
 do
-    for MODEL in 'dlinear' 'gru_nvp' 'patchtst' 'timegrad'
+    for MODEL in 'gru_nvp' 'patchtst' 'timegrad'
     do
         python run.py --config config/m4/${DATASET}/${MODEL}.yaml --seed_everything 0  \
             --data.data_manager.init_args.path ${DATA_DIR} \
