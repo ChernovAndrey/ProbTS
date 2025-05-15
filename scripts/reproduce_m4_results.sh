0e2d1f6560159fcd755e02262108343438817214
Andrei Chernov <chernov.andrey.998@gmail.com>
2025-05-15 14:11:35 +0200

    added seeds for the baselines

--- scripts/reproduce_m4_results.sh
+++ scripts/reproduce_m4_results.sh
@@ -3,12 +3,15 @@ export CUDA_VISIBLE_DEVICES=0
 DATA_DIR=./datasets
 LOG_DIR=./exps
 
-for DATASET in 'm4_daily' 'm4_weekly' 'm5' 'tourism_monthly'
+for SEED in 1 2 3 4 5
 do
-    for MODEL in 'dlinear' 'gru_nvp' 'patchtst' 'timegrad'
+    for DATASET in 'm4_daily' 'm4_weekly' 'tourism_monthly'
     do
-        python run.py --config config/m4/${DATASET}/${MODEL}.yaml --seed_everything 0  \
-            --data.data_manager.init_args.path ${DATA_DIR} \
-            --trainer.default_root_dir ${LOG_DIR}
+        for MODEL in 'dlinear' 'gru_nvp' 'patchtst' 'timegrad'
+        do
+            python run.py --config config/m4/${DATASET}/${MODEL}.yaml --seed_everything ${SEED} \
+                --data.data_manager.init_args.path ${DATA_DIR} \
+                --trainer.default_root_dir ${LOG_DIR}
+        done
     done
 done
