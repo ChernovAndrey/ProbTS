d532a71d3c26111e1b676eec4056a429c9ab3b1c
Andrei Chernov <chernov.andrey.998@gmail.com>
2025-05-16 01:25:14 +0200

    scripts

--- scripts/binconv_m4.sh
+++ scripts/binconv_m4.sh
@@ -1,14 +1,17 @@
 export CUDA_VISIBLE_DEVICES=0
 
 DATA_DIR=./datasets
-LOG_DIR=./exps_binconv
+LOG_DIR=./exps_binconv_seeds
 
-for DATASET in 'm4_daily' 'm4_weekly' 'tourism_monthly'
+for SEED in {1..5}
 do
-    for MODEL in 'binconv'
+    for DATASET in 'm4_daily' 'm4_weekly' 'tourism_monthly'
     do
-        python run.py --config config/m4/${DATASET}/${MODEL}.yaml --seed_everything 0  \
-            --data.data_manager.init_args.path ${DATA_DIR} \
-            --trainer.default_root_dir ${LOG_DIR}
+        for MODEL in 'binconv'
+        do
+            python run.py --config config/m4/${DATASET}/${MODEL}.yaml --seed_everything ${SEED} \
+                --data.data_manager.init_args.path ${DATA_DIR} \
+                --trainer.default_root_dir ${LOG_DIR}
+        done
     done
 done
--- scripts/binconv_prob_m4.sh
+++ scripts/binconv_prob_m4.sh
@@ -1,14 +1,17 @@
 export CUDA_VISIBLE_DEVICES=0
 
 DATA_DIR=./datasets_prob
-LOG_DIR=./exps_binconv_prob
+LOG_DIR=./exps_binconv_prob_seeds
 
-for DATASET in 'm4_daily' 'm4_weekly' 'tourism_monthly'
+for SEED in {1..5}
 do
-    for MODEL in 'binconv_prob'
+    for DATASET in 'm4_daily' 'm4_weekly' 'tourism_monthly'
     do
-        python run.py --config config/m4/${DATASET}/${MODEL}.yaml --seed_everything 0  \
-            --data.data_manager.init_args.path ${DATA_DIR} \
-            --trainer.default_root_dir ${LOG_DIR}
+        for MODEL in 'binconv_prob'
+        do
+            python run.py --config config/m4/${DATASET}/${MODEL}.yaml --seed_everything ${SEED} \
+                --data.data_manager.init_args.path ${DATA_DIR} \
+                --trainer.default_root_dir ${LOG_DIR}
+        done
     done
 done
